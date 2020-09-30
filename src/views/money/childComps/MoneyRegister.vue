d05a39136d83ddb3dd9c9f6fe329df8c121f96b9
孙仕杰 <1007821840@qq.com>
2020-09-30 08:58:13 +0800

    cbuxfisrt

---
+++ src/views/money/childComps/MoneyRegister.vue
@@ -0,0 +1,31 @@
+<template>
+  <div path="/register">
+    <div>
+      <span>使用星巴克App进行注册</span>
+    </div>
+    <div>
+      <p>
+        <span>现在就下载</span>
+        <a href="/apps/">星巴克app</a>
+        <span>便可直接用手机进行注册、支付、查看消费记录及管理卡片。</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"MoneyRegister",
+  data () {
+    return {
+      
+    };
+  },
+  components: {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style scoped>
+  
+</style>
